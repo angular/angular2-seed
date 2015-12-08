06064941302e29376210b6f5711b1c66bbf5a985
Rob Wormald <robwormald@gmail.com>
2015-12-07 22:43:46 -0800

    feature(app): add router and http providers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import 'zone.js';
 import 'reflect-metadata';
+
 import {bootstrap} from 'angular2/angular2';
+import {HTTP_PROVIDERS} from 'angular2/http';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+
+const APP_PROVIDERS = [
+  ROUTER_PROVIDERS,
+  HTTP_PROVIDERS
+];
 
 import {App} from './app/components/app';
 
-bootstrap(App)
+bootstrap(App, [APP_PROVIDERS])
   .catch(err => console.error(err));
