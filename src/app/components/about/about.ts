829bd8a27f25db22325a07953ee6480f754db734
Rob Wormald <robwormald@gmail.com>
2015-12-14 14:05:49 -0800

    feature(app): add about component

---
+++ src/app/components/about/about.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'about',
+  templateUrl: 'app/components/about/about.html',
+  styleUrls: ['app/components/about/about.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class About {
+
+  constructor() {}
+
+}
