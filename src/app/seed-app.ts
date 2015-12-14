90120c29cc19dde6ea98bc47a33008fae97c5a7a
Rob Wormald <robwormald@gmail.com>
2015-12-14 14:03:29 -0800

    feature(app): add root component

---
+++ src/app/seed-app.ts
@@ -0,0 +1,22 @@
+import {Component} from 'angular2/core';
+import {Router, Route, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+import {Home} from './components/home/home';
+import {About} from './components/about/about';
+
+@Component({
+  selector: 'seed-app',
+  providers: [],
+  templateUrl: 'app/seed-app.html',
+  directives: [ROUTER_DIRECTIVES],
+  pipes: []
+})
+@RouteConfig([
+  new Route({ path: '/home', component: Home, name: 'Home', useAsDefault: true}),
+  new Route({ path: '/about', component: About, name: 'About'})
+])
+export class SeedApp {
+
+  constructor() {}
+
+}
