040d4af706cc53d98a141a9bf0da9cc2e68aac1e
Rob Wormald <robwormald@gmail.com>
2015-12-14 14:05:38 -0800

    feature(app): add home component

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'home',
+  templateUrl: 'app/components/home/home.html',
+  styleUrls: ['app/components/home/home.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Home {
+
+  constructor() {}
+
+}
