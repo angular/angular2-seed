7389f9af4a153674b85b2b4a1fa770df10293d12
Shidhin C R <shidhincr@gmail.com>
2015-12-16 00:24:01 +0400

    Using Angular2 2.0.0-beta.0

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
+import {enableProdMode} from 'angular2/core';
 
 import {SeedApp} from './app/seed-app';
 
+enableProdMode();
 bootstrap(SeedApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
   .catch(err => console.error(err));
