58bec8f65bcaba924978b2fe4a8cb398bc0b3d1c
gdi2290 <github@gdi2290.com>
2016-03-08 21:28:11 -0800

    feat(app): default to hash location

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
-import {ROUTER_PROVIDERS} from 'angular2/router';
+import {provide} from 'angular2/core';
+import {ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy} from 'angular2/router';
 
 import {SeedApp} from './app/seed-app';
 
@@ -8,5 +9,6 @@ import {SeedApp} from './app/seed-app';
 bootstrap(SeedApp, [
   HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
+  provide(LocationStrategy, {useClass: HashLocationStrategy})
 ])
 .catch(err => console.error(err));
