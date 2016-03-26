200b45b6deb49afbb946f5cfa94d24c4439c1728
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-03-26 22:19:18 +0100

    fix: add Transitive typings reference
    As mentionned in beta 6 breaking changes:
    Transitive typings are no longer included in the distribution.
    
    If you use --target=es5, you will need to add a line somewhere in your application (for example, at the top of the .ts file where you call bootstrap):
    
    ///<reference path="node_modules/angular2/typings/browser.d.ts"/>

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
+
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
