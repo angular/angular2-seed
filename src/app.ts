14769b1421a101d857ab6ed25c0a08564aff545d
gdi2290 <github@gdi2290.com>
2016-04-07 18:23:13 -0700

    chore: use typings

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,3 @@
-/// <reference path="../node_modules/angular2/typings/browser.d.ts" />
-
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {ROUTER_PROVIDERS} from 'angular2/router';
@@ -7,4 +5,4 @@ import {ROUTER_PROVIDERS} from 'angular2/router';
 import {SeedApp} from './app/seed-app';
 
 bootstrap(SeedApp, [HTTP_PROVIDERS, ROUTER_PROVIDERS])
-  .catch(err => console.error(err));
+.catch(err => console.error(err));
