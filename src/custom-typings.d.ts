20ea51e7b204ebd11bb207cc038143823b5554c4
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-07-12 10:43:04 +0200

    refactor: remove old angular aliases

--- src/custom-typings.d.ts
+++ src/custom-typings.d.ts
@@ -28,24 +28,3 @@ import * as _ from 'lodash'
  * see https://github.com/typings/registry
  *
  */
-
-// Alias legacy import/exports pre rc
-declare module "angular2/core" {
-  export * from '@angular/core';
-}
-
-declare module "angular2/http" {
-  export * from '@angular/http';
-}
-
-declare module 'angular2/platform/browser' {
-  export * from '@angular/platform-browser'
-}
-
-declare module 'angular2/testing' {
-  export * from '@angular/core/testing';
-}
-
-declare module 'angular2/http/testing' {
-  export * from '@angular/http/testing';
-}
