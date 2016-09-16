dfa9323094648ed1ef9b5504845be32ecc0922bd
Tiep Phan <tieppt12@gmail.com>
2016-09-16 12:19:48 +0700

    Add Sass to seed project

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector   : 'app',
   templateUrl: './app.html',
+  styles: [ require('./app.scss') ],
 })
 export class AppComponent {
 }
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,8 @@ var webpackConfig = {
       // .ts files for TypeScript
       { test: /\.ts$/, loaders: ['awesome-typescript-loader', 'angular2-template-loader'] },
       { test: /\.css$/, loaders: ['to-string-loader', 'css-loader'] },
-      { test: /\.html$/, loader: 'raw-loader' }
+      { test: /\.html$/, loader: 'raw-loader' },
+      { test: /\.scss$/, loaders: ['raw-loader', 'sass-loader'] }
     ]
   }
 
