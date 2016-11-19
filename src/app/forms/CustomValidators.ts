b355a29d4c796e5a7f2fdcfee5e5a94e4168bfa8
Tiep Phan <tieppt@users.noreply.github.com>
2016-11-19 10:39:13 +0700

    feat: add contact form using Reactive Forms Module (#141)

---
+++ src/app/forms/CustomValidators.ts
@@ -0,0 +1,16 @@
+import {FormControl} from '@angular/forms';
+
+export default class CustomValidators {
+  /**
+   * sample from http://blog.thoughtram.io/angular/2016/03/14/custom-validators-in-angular-2.html
+   */
+  static validateEmail(c: FormControl) {
+    let EMAIL_REGEXP = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+    return EMAIL_REGEXP.test(c.value) ? null : {
+      validateEmail: {
+        valid: false
+      }
+    };
+  }
+}
