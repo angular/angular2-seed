9f33b2be29909cdb9325014c57568aac4a5428ca
PatrickJS <github@gdi2290.com>
2016-11-18 22:41:45 -0500

    refactor(main.browser): check if ready

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,5 +1,17 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+export const platformRef = platformBrowserDynamic();
+
+export function main() {
+  reutrn platformRef.bootstrapModule(AppModule)
+  .catch(err => console.error(err));  
+}
+
+// support async tag or hmr
+if (document.readyState === 'complete') {
+  main()
+} else {
+  document.addEventListener('DOMContentLoaded', () => main());
+}
+
