7496ddb8ab2d204f712bc4fd3028e784a5df7e6c
Peter de Croos <cultofmetatron@aumlogic.com>
2016-12-09 16:21:46 -0800

    added cors headers to devpack
    since --cors no longer works, I added the following here so that we can still have cors

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,12 @@ var defaultConfig = {
 
   devServer: {
     historyApiFallback: true,
-    watchOptions: { aggregateTimeout: 300, poll: 1000 }
+    watchOptions: { aggregateTimeout: 300, poll: 1000 },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
   },
 
   node: {
